534a15c8440f9de31cc796fc75ec51030f6f6e5f
Nghia.Bui <buitrungnghia1.11@gmail.com>
2020-11-05 08:58:23 +0700

    second

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,37 @@ import java.util.Scanner;
 public class Main {
     int choice;
     String str = "";
-    public void draw(){
-        switch (choice){
-            case 1 :
-                for(int i = 0; i < 5; i++){
+
+    public void draw() {
+        switch (choice) {
+            case 1:
+                for (int i = 0; i < 5; i++) {
                     str += "*";
                     System.out.println(str);
                 }
                 break;
+            case 2:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 3:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+                break;
+            default:
+                break;
         }
     }
-    public void getInfo(){
+
+    public void getInfo() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1 - draw triangle");
         System.out.println("Enter 2 - draw square");
@@ -21,6 +41,7 @@ public class Main {
         System.out.println("Enter 0 - Exit");
         choice = sc.nextInt();
     }
+
     public static void main(String[] args) {
         Main m = new Main();
         m.getInfo();
