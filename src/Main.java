23d7ee6c916a82454c810ecfb93312e8163dafe3
Nghia.Bui <buitrungnghia1.11@gmail.com>
2020-11-05 08:34:27 +0700

    .

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    int choice;
+    String str = "";
+    public void draw(){
+        switch (choice){
+            case 1 :
+                for(int i = 0; i < 5; i++){
+                    str += "*";
+                    System.out.println(str);
+                }
+                break;
+        }
+    }
+    public void getInfo(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 - draw triangle");
+        System.out.println("Enter 2 - draw square");
+        System.out.println("Enter 3 - draw rectangle");
+        System.out.println("Enter 0 - Exit");
+        choice = sc.nextInt();
+    }
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.getInfo();
+        m.draw();
+
+    }
+}
